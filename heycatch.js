6b5dfcdccf2c4d3ec264c13c15e2ea97a41c4bf3
tinyelvis1 <theradish@gmail.com>
2026-09-11 16:13:53 -0400

    Add pinned HeyCatch module and short-link 404 forward

---
+++ heycatch.js
@@ -0,0 +1,6 @@
+import { analytics } from "https://esm.sh/@heycatch/sdk@0.7.0";
+
+analytics.init({
+  projectKey: "hck_pk_3qpEDIthx4rdspq0KquwL9QPNTBDKzHh",
+  install: { framework: "web", agent: "other" },
+});
